74809e001a8021647d389a9a776f9a4f930c3ac9
Jay Janssen <jay.janssen@gmail.com>
2012-05-17 10:48:09 -0400

    Added xinet to all hosts, haproxy to percona1, removed glbd

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,19 @@
 node percona1 {
 	include percona::repository
 	include percona::cluster
+	include xinet
 	include myhosts
+	include haproxy
 
-	Class['percona::repository'] -> Class['percona::cluster'] -> Class['galera::glb']
+	Class['percona::repository'] -> Class['percona::cluster']
 
-	class {
-		'galera::glb':
-			glb_list_backend => "192.168.70.2:3306:1 192.168.70.3:3306:1 192.168.70.4:3306"
-	}
+
 }
 
 node percona2 {
 	include percona::repository
 	include percona::cluster
+	include xinet
 	include myhosts
 
 	Class['percona::repository'] -> Class['percona::cluster']
@@ -23,6 +23,7 @@ node percona2 {
 node percona3 {
 	include percona::repository
 	include percona::cluster
+  include xinet
 	include myhosts
 
 	Class['percona::repository'] -> Class['percona::cluster']
