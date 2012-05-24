7deb713a23659f338d88eec3db86047bbce24473
Jay Janssen <jay.janssen@gmail.com>
2012-05-24 13:10:55 -0400

    add percona toolkit to all installs, fixed toolkit package to rely on the yum repo

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,31 +1,37 @@
 node percona1 {
 	include percona::repository
 	include percona::cluster
+	include percona::toolkit	
 	include xinet
 	include myhosts
 	include haproxy
 
 	Class['percona::repository'] -> Class['percona::cluster']
-
+	Class['percona::repository'] -> Class['percona::toolkit']
 
 }
 
 node percona2 {
 	include percona::repository
 	include percona::cluster
+	include percona::toolkit	
 	include xinet
 	include myhosts
 
 	Class['percona::repository'] -> Class['percona::cluster']
+	Class['percona::repository'] -> Class['percona::toolkit']
 
 }
 
 node percona3 {
 	include percona::repository
 	include percona::cluster
+	include percona::toolkit	
   include xinet
 	include myhosts
 
 	Class['percona::repository'] -> Class['percona::cluster']
+	Class['percona::repository'] -> Class['percona::toolkit']
+	
 }
 
--- modules/percona/manifests/toolkit.pp
+++ modules/percona/manifests/toolkit.pp
@@ -8,10 +8,8 @@ class percona::toolkit {
 		"perl-DBD-MySQL":
 			ensure  => installed,
 			require => Package['Percona-Server-shared-compat'];
-        	"percona-toolkit":
-                	provider => rpm,
-                	ensure   => installed,
-			require  => [ Package['perl-Time-HiRes'], Package['perl-TermReadKey'], Package['perl-DBD-MySQL'] ],
-                	source   => "http://www.percona.com/redir/downloads/percona-toolkit/percona-toolkit-1.0.1-1.noarch.rpm";
-	}
+    "percona-toolkit":
+    	ensure   => installed,
+			require  => [ Package['perl-Time-HiRes'], Package['perl-TermReadKey'], Package['perl-DBD-MySQL'] ];
+		}
 }
