d5bdc4c3a278a9f9b35d72c67ef903e79184768d
Jay Janssen <jay.janssen@gmail.com>
2013-02-05 16:16:37 -0500

    update dependency tree and package aliases for clean 5.5.29 install

--- modules/percona/manifests/cluster/packages.pp
+++ modules/percona/manifests/cluster/packages.pp
@@ -1,21 +1,22 @@
 class percona::cluster::packages {
 
-        package {
-                "Percona-XtraDB-Cluster-server.$hardwaremodel":
-                        alias => "MySQL-server",
-                        require => [ Yumrepo['percona'], Package['MySQL-client'] ],
-                        ensure => "installed";
-                "Percona-XtraDB-Cluster-client.$hardwaremodel":
-                        alias => "MySQL-client",
-                        require => Yumrepo['percona'],
-                        ensure => "installed";
-                "rsync":
-                        ensure => "present";
+	package {
+		"Percona-XtraDB-Cluster-server.$hardwaremodel":
+			alias => "MySQL-server",
+			require => [ Yumrepo['percona'], Package['MySQL-client'], Package['MySQL-shared-compat'] ],
+			ensure => "installed";
+		"Percona-XtraDB-Cluster-client.$hardwaremodel":
+			alias => "MySQL-client",
+			require => [ Yumrepo['percona'], Package['MySQL-shared-compat'] ],
+			ensure => "installed";
+		"rsync":
+			ensure => "present";
 		"mysql-libs":
 			ensure => "absent";		
 		"Percona-Server-shared-compat":
-			require => [ Yumrepo['percona'], Package['mysql-libs'], Package['MySQL-client'] ],
+			alias => "MySQL-shared-compat",
+			require => [ Yumrepo['percona'], Package['mysql-libs'] ],
 			ensure => "installed";
 
-        }
+	}
 }
