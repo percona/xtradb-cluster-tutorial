667693feddc45726889597b86fbd5da7c72c651d
Jay Janssen <jay.janssen@gmail.com>
2013-02-05 16:21:37 -0500

    created bare_cluster and full_cluster manifests

--- manifests/full_cluster.pp
+++ manifests/full_cluster.pp
@@ -2,9 +2,9 @@ node node1 {
 	include percona::repository
 	include percona::cluster
 	include percona::toolkit	
-	include xinet
+	# include xinet
 	include misc
-	include haproxy
+	# include haproxy
 
 	Class['percona::repository'] -> Class['percona::cluster']
 	Class['percona::repository'] -> Class['percona::toolkit']
@@ -15,7 +15,7 @@ node node2 {
 	include percona::repository
 	include percona::cluster
 	include percona::toolkit	
-	include xinet
+	# include xinet
 	include misc
 
 	Class['percona::repository'] -> Class['percona::cluster']
@@ -27,7 +27,7 @@ node node3 {
 	include percona::repository
 	include percona::cluster
 	include percona::toolkit	
-  include xinet
+  # include xinet
 	include misc
 
 	Class['percona::repository'] -> Class['percona::cluster']
