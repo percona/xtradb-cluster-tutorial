73d486a3777ce7d1aa6006503af0addecfd33360
Jay Janssen <jay.janssen@gmail.com>
2013-09-12 10:51:04 -0400

    got pxc bootstrap working

---
+++ puppet/manifests/pxc.pp
@@ -0,0 +1,15 @@
+include percona::repository
+include percona::toolkit
+include percona::cluster
+
+include misc
+include misc::mysql_datadir
+include misc::sysbench
+include misc::local_percona_repo
+
+Class['misc'] -> Class['percona::repository']
+
+Class['misc::mysql_datadir'] -> Class['percona::cluster']
+
+Class['percona::repository'] -> Class['percona::cluster']
+Class['percona::repository'] -> Class['percona::toolkit']
